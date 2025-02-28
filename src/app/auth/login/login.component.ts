f82e87bcfbf9f125bba8a61b9f9fab86f0252d81
oyewolz <oyewoletaiwo219@gmail.com>
2025-02-27 22:23:56 -0700

    Refactor login component: enhance UI with a login card, add signup link, and improve error message display

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
